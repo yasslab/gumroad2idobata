4a9134a2caf54de1ac45b8fa10c02ca53dd7db69
nanophate <z.nanophate@icloud.com>
2016-03-21 22:36:56 +0900

    add memo

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ Idobata.hook_url = ENV['IDOBATA_END']
 #OmniAuthの使い方がわからないため、tokenを利用した
 gumroad_access_token = ENV['GUMROAD_ACCESS_TOKEN']
 
-target_url = "https://api.gumroad.com/v2/products/?gumroad_access_token="
+target_url  = "https://api.gumroad.com/v2/products/?gumroad_access_token="
 base_url	= target_url + "#{@gumroad_access_token}"
 
 uri  = open(base_url)
@@ -21,7 +21,10 @@ json = JSON.parse(data)	#extract as json
 total_sales = json['products'][0]['sales_count']	#looking for field and putting the value as var
 
 Idobata::Message.create(source:"Total Sales:" + total_sales) 	#combing and making the sentence
+
 #Problem
 # => I need help in oauth
 #IDEA
 # => IF the total sales is equal from previous run; break? excep.?
+#TO DO
+# => Ask for heroku account and how to set ENV on my computer
