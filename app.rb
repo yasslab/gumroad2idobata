297f7be7573a45bbb3f10e64a2dc0250603e4507
nanophate <z.nanophate@icloud.com>
2016-03-21 21:10:56 +0900

    set required env and hook for idobata

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'open-uri'
+require 'json'
+require 'idobata'
+#require 'omniauth-gumroad'
+
+#use OmniAuth::Builder do
+#    provider :gumroad, ENV['GUMROAD_APPLICATION_ID'], ENV['GUMROAD_APP_SECRET']
+#end
+
+Idobata.hook_url = ENV['IDOBATA_END']
+#OmniAuthの使い方がわからないため、tokenを利用した
+gumroad_access_token = ENV['GUMROAD_ACCESS_TOKEN']
