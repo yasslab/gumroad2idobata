e8774d63f7707235a48540327190aeabba9500f5
nanophate <z.nanophate@icloud.com>
2016-03-25 16:05:44 +0900

    add a better wording to send a message through idobata

--- app.rb
+++ app.rb
@@ -14,12 +14,12 @@ uri  = open(base_url)
 data = uri.read
 json = JSON.parse(data)	#extract as json
 
-prev_tsales	= total_sales							#storing previous var to diffrent var
-total_sales = json['products'][0]['sales_count']	#looking for field and putting the value as var
-tsales_diff = total_sales - prev_tsales				#subtacting the diffrence between old and new var
+prev_tsales	= @total_sales							#storing previous var to diffrent var
+@total_sales = json['products'][0]['sales_count']	#looking for field and putting the value as var
+tsales_diff = @total_sales.to_i - prev_tsales.to_i				#subtacting the diffrence between old and new var
 
-#if prev_tsales < total_sales
-#	emoji = :naku
-#end
+if prev_tsales < total_sales
+	emoji = :naku
+end
 
-Idobata::Message.create(source:"今日の売上は、¥" + total_sales +"円です！" + "前回の売上との差額は" + tsales_diff) 	#combing and making the sentence
+Idobata::Message.create(source:"今日の売上は、#{@total_sales}円です！ 前回の売上との差額は #{tsales_diff}円です！") 	#combing and making the sentence
