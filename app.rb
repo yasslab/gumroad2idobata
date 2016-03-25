2aca8e8ef54f259acdfd40c0f2391674109e0cec
nanophate <z.nanophate@icloud.com>
2016-03-25 23:13:17 +0900

    Add url to get json from Gumroad and make the data useable

--- app.rb
+++ app.rb
@@ -6,3 +6,6 @@ require 'idobata'
 Idobata.hook_url = ENV['IDOBATA_END']
 gumroad_access_token = ENV['GUMROAD_ACCESS_TOKEN']
 
+uri  = open(base_url)
+data = uri.read
+json = JSON.parse(data)    #extract as json
