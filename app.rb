fa7c43d61c3bda460988e9e1b0a9c24c0052e63e
nanophate <z.nanophate@icloud.com>
2016-03-28 18:40:29 +0900

    Change code for better api handling and scalibility

--- app.rb
+++ app.rb
@@ -5,20 +5,29 @@ require 'idobata'
 
 Idobata.hook_url = ENV['IDOBATA_END']
 gumroad_access_token = ENV['GUMROAD_ACCESS_TOKEN']
-
 target_url  = "https://api.gumroad.com/v2/products/?access_token="
 base_url    = target_url + "#{gumroad_access_token}"
 
 uri  = open(base_url)
 data = uri.read
-json = JSON.parse(data)    #extract as json
+json = JSON.parse(data)    				#extract as json
 
-prev_tsales    = @total_sales                        #storing previous var to diffrent var
-@total_sales = json['products'][0]['sales_count']    #looking for field and putting the value as var
+prev_tsales = @total_sales_sum 			#storing previous var to diffrent var
+total_sales = [] 						#make an array list to store each product's sales count
+total_items = json['products'].length	#get the array value stored under products
 
-emoji = ":moneybag:"
+#Loop each product array to get sales_count
+total_items.times do |x|
+	total_sales << json['products'][x]['sales_count']
+end
+
+@total_sales_sum = total_sales.inject(0, :+)  #add each array value and store as a sum
+
+#Dealing with emoji's being sent 
+emoji 		= ":moneybag:"
 emoji_array = []
-@total_sales.times do |x|
+
+@total_sales_sum.times do |a|
     emoji_array << emoji
 end
 
