2829f186adad1d0143f98916a57c75ec6dd2364c
nanophate <z.nanophate@icloud.com>
2016-04-11 22:08:12 +0900

    Fix code to output emoji and to loop through each page

--- app.rb
+++ app.rb
@@ -3,17 +3,38 @@ require 'date'
 require 'json'
 require 'idobata'
 require 'pry'
+require './art.rb'
 
 Idobata.hook_url	= ENV['IDOBATA_END']
-Idobata.hook_url	= ENV['IDOBATA_LOCAL_END']
+#Idobata.hook_url	= ENV['IDOBATA_LOCAL_END']
 gumroad_access_token	= ENV['GUMROAD_ACCESS_TOKEN']
 
-target_url  = "https://api.gumroad.com/v2/sales/?access_token="
-base_url    = target_url + "#{gumroad_access_token}&before=#{Date.today}&after=#{Date.today - 1}" 
 
-base_url    = target_url + "#{gumroad_access_token}&before=2016-3-31&after=2015-10-1" 
+target_url		= "https://api.gumroad.com/v2/sales/?access_token="
+base_url		= target_url + "#{gumroad_access_token}&before=#{Date.today}&after=#{Date.today - 1}"
+#base_url		= target_url + "#{gumroad_access_token}&before=2016-4-10&after=2016-3-1"
 
-json = JSON.parse(open(base_url) { |io| data = io.read })
-sale = json['sales'].inject(0){|sum, product| sum + product['price']}
+jsonCount = JSON.parse(open(base_url) { |io| data = io.read })
 
-Idobata::Message.create(source:"今日の購入数は#{json['sales'].size}")
+if jsonCount['next_page_url'].nil? == false
+	i = 1
+	
+	@jsonAll = JSON.parse(open(base_url + "&page=#{i}") { |io| data = io.read})
+	while	@jsonAll['next_page_url'].nil? == false 
+		@jsonAll = JSON.parse(open(base_url + "&page=#{i}") { |io| data = io.read})	
+		i += 1
+	end
+
+jsonFinalCount	=	JSON.parse(open(base_url+ "&page=#{i - 1}") { |io| data = io.read })
+total 		= 	jsonFinalCount['sales'].size + ((i-2)*10)
+
+Idobata::Message.create(source:"設定された日付からの集計は #{total}")
+
+else if jsonCount['next_page_url'].nil? == true
+
+emoji_readable = []
+@body = uriage(jsonCount['sales'].size.to_s)
+emoji_readable  << @body
+Idobata::Message.create(source:"今日の購入数は " + "\n" + "#{emoji_readable*"\n"}")
+end
+end
