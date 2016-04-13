b8a0087b650ea1c0a995de46c93bb4c213e5d469
Yohei YASUKAWA <yasulab@gmail.com>
2016-04-13 17:10:33 +0900

    Add sample test using Test::Unit

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 # ruby '2.3.0'
 
 gem 'idobata'
+gem 'test-unit'
---
+++ test_sample.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+require 'open-uri'
+require 'json'
+
+
+class TestSample < Test::Unit::TestCase
+  def setup
+    gumroad_access_token  = ENV['GUMROAD_ACCESS_TOKEN']
+    base_url   = "https://api.gumroad.com/v2/sales/?access_token="
+    @target_url = base_url + "#{gumroad_access_token}&before=2016-03-31&after=2016-01-01"
+  end
+
+  def test_gumroad_api
+    sales = JSON.parse(open(@target_url) {|uri| uri.read})
+    p sales
+    assert_false sales['next_page_url'].nil?
+  end
+end
