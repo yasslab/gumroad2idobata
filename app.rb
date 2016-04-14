0cae9ce132e46538699d70b7dbd78f748c33ddda
nanophate <nanophate@me.com>
2016-04-14 22:41:24 +0900

    Fix typo and variable mismatch

--- app.rb
+++ app.rb
@@ -10,11 +10,11 @@ class Gumroad
   def initialize(token)
     @base_url  = "https://api.gumroad.com"
     @token_url = "&access_token=#{token}"
-    @start_page_url = "/v2/sales/?before=#{Date.today}&after=#{Date.today - 1}"
+    @start_page_url = "/v2/sales/?before=#{Date.today}&after=#{Date.today - 100}"
   end
 
-  def sales_data_getter(pager_url)
-    page_url ||= @first_page_url  #same as .replace? ask @yasulab
+  def sales_page_getter(page_url)
+    page_url ||= @start_page_url  #same as .replace? ask @yasulab
     target_url = @base_url  + page_url + @token_url
     JSON.parse(open(target_url).read)
   end
@@ -27,8 +27,8 @@ sales_item_data = {} #{} => hash [] => array
 sales_count = 0
 
 loop do
-  sales_item_data = gumroad.sales_data_getter(sales_item_data['next_page_url'])
-  sales_count += sales_item_data['next_page_url'].size
+  sales_item_data = gumroad.sales_page_getter(sales_item_data['next_page_url'])
+  sales_count += sales_item_data['sales'].size
   break if sales_item_data['next_page_url'].nil?
 end
 
