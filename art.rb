245fe3a0cce16c9535e1e66192b3977671288fa1
nanophate <nanophate@me.com>
2016-04-15 15:03:32 +0900

    Add spacing and changed layout

--- art.rb
+++ art.rb
@@ -1,10 +1,10 @@
 def uriage(sales_number)
-  emojis[sales_number]
+  @emojis[sales_number]
 end
 
-emojis = Hash.new
+@emojis = Hash.new
 
-emojis[9] = "
+@emojis[9] = "
   :cloud::moneybag::moneybag::moneybag::cloud::cloud:
   :moneybag::cloud::cloud::cloud::moneybag::cloud:
   :moneybag::cloud::cloud::cloud::moneybag::cloud:
@@ -15,93 +15,93 @@ emojis[9] = "
   "
 
 
-emojis[8] = "
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-"
+@emojis[8] = "
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  "
 
-emojis[7] = "
-:moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
-:moneybag::cloud::cloud::moneybag::cloud::cloud:
-:cloud::cloud::moneybag::cloud::cloud::cloud:
-:cloud::moneybag::cloud::cloud::cloud::cloud:
-:cloud::moneybag::cloud::cloud::cloud::cloud:
-:cloud::moneybag::cloud::cloud::cloud::cloud:
-:cloud::cloud::cloud::cloud::cloud::cloud:
-"
+@emojis[7] = "
+  :moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
+  :moneybag::cloud::cloud::moneybag::cloud::cloud:
+  :cloud::cloud::moneybag::cloud::cloud::cloud:
+  :cloud::moneybag::cloud::cloud::cloud::cloud:
+  :cloud::moneybag::cloud::cloud::cloud::cloud:
+  :cloud::moneybag::cloud::cloud::cloud::cloud:
+  :cloud::cloud::cloud::cloud::cloud::cloud:
+  "
 
 
-emojis[6] = "
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:moneybag::cloud::cloud::cloud::cloud::cloud:
-:moneybag::moneybag::moneybag::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-"
+@emojis[6] = "
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :moneybag::cloud::cloud::cloud::cloud::cloud:
+  :moneybag::moneybag::moneybag::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  "
 
-emojis[5] = "
-:moneybag::moneybag:::moneybag::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::cloud::cloud::cloud:
-:moneybag::cloud::cloud::cloud::cloud::cloud:
-:moneybag::moneybag::moneybag::moneybag::cloud::cloud:
-:cloud::cloud::cloud::cloud::moneybag::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-"
+@emojis[5] = "
+  :moneybag::moneybag:::moneybag::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::cloud::cloud: 
+  :moneybag::cloud::cloud::cloud::cloud::cloud:
+  :moneybag::moneybag::moneybag::moneybag::cloud::cloud:
+  :cloud::cloud::cloud::cloud::moneybag::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  "
 
-emojis[4] = "
-:moneybag::cloud::cloud::cloud::cloud::cloud:
-:moneybag::cloud::cloud::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::moneybag::cloud::cloud:
-:moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
-:cloud::cloud::cloud::moneybag::cloud::cloud:
-:cloud::cloud::cloud::moneybag::cloud::cloud:
-"
+@emojis[4] = "
+  :moneybag::cloud::cloud::cloud::cloud::cloud:
+  :moneybag::cloud::cloud::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::moneybag::cloud::cloud:
+  :moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
+  :cloud::cloud::cloud::moneybag::cloud::cloud:
+  :cloud::cloud::cloud::moneybag::cloud::cloud:
+  "
 
-emojis[3] = "
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:cloud::cloud::cloud::cloud::moneybag::cloud:
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-:cloud::cloud::cloud::cloud::moneybag::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-"
+@emojis[3] = "
+ :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  :cloud::cloud::cloud::cloud::moneybag::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  "
 
-emojis[2] = "
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::cloud::moneybag::cloud:
-:cloud::cloud::cloud::cloud::moneybag::cloud:
-:cloud::moneybag::moneybag::moneybag::cloud::cloud:
-:moneybag::cloud::cloud::cloud::cloud::cloud:
-:moneybag::cloud::cloud::cloud::cloud::cloud:
-:moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
-"
+@emojis[2] = "
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::cloud::cloud:
+  :moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
+  "
 
-emojis[1] = "
-:cloud::cloud::moneybag::cloud::cloud::cloud:
-:cloud::moneybag::moneybag::cloud::cloud::cloud:
-:moneybag::cloud::moneybag::cloud::cloud::cloud:
-:cloud::cloud::moneybag::cloud::cloud::cloud:
-:cloud::cloud::moneybag::cloud::cloud::cloud:
-:cloud::cloud::moneybag::cloud::cloud::cloud:
-:moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
-"
+@emojis[1] = "
+  :cloud::cloud::moneybag::cloud::cloud::cloud:
+  :cloud::moneybag::moneybag::cloud::cloud::cloud:
+  :moneybag::cloud::moneybag::cloud::cloud::cloud:
+  :cloud::cloud::moneybag::cloud::cloud::cloud:
+  :cloud::cloud::moneybag::cloud::cloud::cloud:
+  :cloud::cloud::moneybag::cloud::cloud::cloud:
+  :moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
+  "
 
-emojis[0] = "
-:cloud::cloud::money::cloud::cloud::cloud:
-:cloud::monry::cloud::money::cloud::cloud:
-:money::cloud::cloud::cloud::money::cloud:
-:money::cloud::cloud::cloud::money::cloud:
-:money::cloud::cloud::cloud::money::cloud:
-:cloud::money::cloud::money::cloud::cloud:
-:cloud::cloud::money::cloud::cloud::cloud:
-"
+@emojis[0] = "
+  :cloud::cloud::moneybag::cloud::cloud::cloud:
+  :cloud::moneybag::cloud::moneybag::cloud::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :moneybag::cloud::cloud::cloud::moneybag::cloud:
+  :cloud::moneybag::cloud::moneybag::cloud::cloud:
+  :cloud::cloud::moneybag::cloud::cloud::cloud:
+  "
