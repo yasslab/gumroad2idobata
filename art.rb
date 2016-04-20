5a812d9ba6f3b063c632c0b88eeec0276ed96a7b
Yohei YASUKAWA <yasulab@gmail.com>
2016-04-20 21:11:01 +0900

    Refactor emojis by using str2art method

--- art.rb
+++ art.rb
@@ -2,106 +2,116 @@
 #Scroll Down for uriage method
 
 emojis = Hash.new
+C = ":cloud:"
+M = ":moneybag:"
 
-emojis[9] = "
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::moneybag::moneybag::moneybag::cloud:
-  :cloud::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  "
+def str2art(str)
+  str.gsub!("C", ":cloud:")
+     .gsub!("M", ":moneybag:")
+     .gsub!("_", "")
+     .gsub!(" ", "")
+end
 
-emojis[8] = "
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  "
+emojis[9] = str2art("
+  C_M_M_M_C_C
+  M_C_C_C_M_C
+  M_C_C_C_M_C
+  C_M_M_M_M_C
+  C_C_C_C_M_C
+  M_C_C_C_M_C
+  C_M_M_M_C_C
+  ")
 
-emojis[7] = "
-  :moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
-  :moneybag::cloud::cloud::moneybag::cloud::cloud:
-  :cloud::cloud::moneybag::cloud::cloud::cloud:
-  :cloud::moneybag::cloud::cloud::cloud::cloud:
-  :cloud::moneybag::cloud::cloud::cloud::cloud:
-  :cloud::moneybag::cloud::cloud::cloud::cloud:
-  :cloud::cloud::cloud::cloud::cloud::cloud:
-  "
+emojis[8] = str2art("
+  C_M_M_M_C_C
+  M_C_C_C_M_C
+  M_C_C_C_M_C
+  C_M_M_M_C_C
+  M_C_C_C_M_C
+  M_C_C_C_M_C
+  C_M_M_M_C_C
+  ")
 
-emojis[6] = "
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::cloud::cloud:
-  :moneybag::moneybag::moneybag::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  "
+emojis[7] = str2art("
+  M_M_M_M_M_C
+  M_C_C_M_C_C
+  C_C_M_C_C_C
+  C_M_C_C_C_C
+  C_M_C_C_C_C
+  C_M_C_C_C_C
+  C_C_C_C_C_C
+  ")
 
-emojis[5] = "
-  :moneybag::moneybag:::moneybag::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::cloud::cloud: 
-  :moneybag::cloud::cloud::cloud::cloud::cloud:
-  :moneybag::moneybag::moneybag::moneybag::cloud::cloud:
-  :cloud::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  "
+emojis[6] = str2art("
+  C_M_M_M_C_C
+  M_C_C_C_M_C
+  M_C_C_C_C_C
+  M_M_M_M_C_C
+  M_C_C_C_M_C
+  M_C_C_C_M_C
+  C_M_M_M_C_C
+  ")
 
-emojis[4] = "
-  :moneybag::cloud::cloud::cloud::cloud::cloud:
-  :moneybag::cloud::cloud::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::moneybag::cloud::cloud:
-  :moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
-  :cloud::cloud::cloud::moneybag::cloud::cloud:
-  :cloud::cloud::cloud::moneybag::cloud::cloud:
-  "
+emojis[5] = str2art("
+  M_M_M_M_M_C
+  M_C_C_C_C_C
+  M_C_C_C_C_C
+  M_M_M_M_C_C
+  C_C_C_C_M_C
+  M_C_C_C_M_C
+  C_M_M_M_C_C
+  ")
 
-emojis[3] = "
- :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  :cloud::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  "
+emojis[4] = str2art("
+  M_C_C_C_C_C
+  M_C_C_M_C_C
+  M_C_C_M_C_C
+  M_C_C_M_C_C
+  M_M_M_M_M_C
+  C_C_C_M_C_C
+  C_C_C_M_C_C
+  ")
 
-@emojis[2] = "
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::moneybag::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::cloud::cloud:
-  :moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
-  "
+emojis[3] = str2art("
+  C_M_M_M_C_C
+  M_C_C_C_M_C
+  C_C_C_C_M_C
+  C_M_M_M_C_C
+  C_C_C_C_M_C
+  M_C_C_C_M_C
+  C_M_M_M_C_C
+  ")
 
-emojis[1] = "
-  :cloud::cloud::moneybag::cloud::cloud::cloud:
-  :cloud::moneybag::moneybag::cloud::cloud::cloud:
-  :moneybag::cloud::moneybag::cloud::cloud::cloud:
-  :cloud::cloud::moneybag::cloud::cloud::cloud:
-  :cloud::cloud::moneybag::cloud::cloud::cloud:
-  :cloud::cloud::moneybag::cloud::cloud::cloud:
-  :moneybag::moneybag::moneybag::moneybag::moneybag::cloud:
-  "
+emojis[2] = str2art("
+  C_M_M_M_C_C
+  M_C_C_C_M_C
+  C_C_C_C_M_C
+  C_M_M_M_C_C
+  M_C_C_C_C_C
+  M_C_C_C_C_C
+  M_M_M_M_M_C
+  ")
 
-emojis[0] = "
-  :cloud::cloud::moneybag::cloud::cloud::cloud:
-  :cloud::moneybag::cloud::moneybag::cloud::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :moneybag::cloud::cloud::cloud::moneybag::cloud:
-  :cloud::moneybag::cloud::moneybag::cloud::cloud:
-  :cloud::cloud::moneybag::cloud::cloud::cloud:
-  "
-  define_method :uriage do |sales|
-    emojis[sales]
-  end
+emojis[1] = str2art("
+  C_C_M_C_C_C
+  C_M_M_C_C_C
+  M_C_M_C_C_C
+  C_C_M_C_C_C
+  C_C_M_C_C_C
+  C_C_M_C_C_C
+  M_M_M_M_M_C
+  ")
+
+emojis[0] = str2art("
+  C_M_M_M_C_C
+  M_C_C_C_M_C
+  M_C_C_C_M_C
+  M_C_C_C_M_C
+  M_C_C_C_M_C
+  M_C_C_C_M_C
+  C_M_M_M_C_C
+  ")
+
+define_method :uriage do |sales|
+  emojis[sales]
+end
