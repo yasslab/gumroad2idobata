5b30376f52c68caca8e161d242b1d32b7b217e49
Yohei YASUKAWA <yasulab@gmail.com>
2016-06-26 12:20:17 +0900

    Add emoji style output and use it by default

--- app.rb
+++ app.rb
@@ -3,8 +3,12 @@ require 'open-uri'
 require 'date'
 require 'json'
 require 'idobata'
-#require 'pry'
+require 'pry'
 require './art.rb'
+
+EMOJI_EYES = ":eyes: "
+EMOJI_BAGS = ":moneybag: "
+OUTPUT_STYLE = :emojis   # or :art
 
 class Gumroad
   def initialize(token)
@@ -14,7 +18,7 @@ class Gumroad
   end
 
   def sales_page_get(page_url)
-    page_url ||= @start_page_url 
+    page_url ||= @start_page_url
     target_url = @base_url  + page_url + @token_url
     JSON.parse(open(target_url).read)
   end
@@ -23,13 +27,30 @@ end
 gumroad = Gumroad.new(ENV['GUMROAD_ACCESS_TOKEN'])
 Idobata.hook_url = ENV['IDOBATA_END']
 
-sales_data = {}  #example: {"success":true,"sales":[....],"next_page_url":[]}
-sales_count = 0
+sales_data   = {}  #example: {"success":true,"sales":[....],"next_page_url":[]}
+sales_count  = 0
+sales_emojis = ""
 
 loop do
   sales_data = gumroad.sales_page_get(sales_data['next_page_url'])
   sales_count += sales_data['sales'].size
-  break if sales_data['next_page_url'].nil? 
+  sales_data['sales'].each do |sale|
+    if (unit = sale['price'].round(-3) / 1000) == 0
+      sales_emojis << EMOJI_EYES
+    else
+      sales_emojis << EMOJI_BAGS * unit
+    end
+  end
+  break if sales_data['next_page_url'].nil?
 end
 
-Idobata::Message.create(source:"#{uriage(sales_count)}")
+if OUTPUT_STYLE == :emojis
+  Idobata::Message.create(source: sales_emojis)
+elsif OUTPUT_STYLE == :art
+  Idobata::Message.create(source: "#{uriage(sales_count)}")
+else
+  # Use emoji style by default
+  Idobata::Message.create(source: sales_emojis)
+end
+
+
